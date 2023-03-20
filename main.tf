b15db048db6a37110ac33ddb91c8e35df0d936e6
Jean1804 <106489575+Jean1804@users.noreply.github.com>
2023-03-20 19:32:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "example" {
     ami = "ami-005f9685cb30f234b"
     instance_type = "t2.micro"
     security_groups = ["default"]
+    key_name = "Accesskey"
     tags = {
         Name = "Instance Terraform"
     }  
